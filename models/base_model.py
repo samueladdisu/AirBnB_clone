205ba03aee2a2bac1b72d3be55ea62afee3d2cfe
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-08-05 20:46:54 +0200

    updated datetime in basemodel.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
 
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
     
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the instance"""
