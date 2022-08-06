e88a3462160cf832f8ec2bfb486d31cef9078f62
samueladdisu <samueladdisu7@gmail.com>
2022-08-07 00:11:48 +0300

    file storage 2

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,12 @@ This is the base model documentation
 """
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """This is the base model from which all models will come from"""
+
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
@@ -20,6 +22,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """prints the class name, id and the __dict of the instance"""
@@ -29,7 +32,8 @@ class BaseModel:
         """
         updates the public instance attribute updated_at
         """
-        self.updated_at = datetime.now()
+        setattr(self, "updated_at",  datetime.now())
+        storage.save()
 
     def to_dict(self):
         dic = {}
