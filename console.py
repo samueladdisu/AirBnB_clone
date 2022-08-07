b75094dbcea7a0a649bd81fabbc7d443003a3849
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-08-07 15:41:14 +0200

    added to console.py

--- console.py
+++ console.py
@@ -7,9 +7,27 @@ import cmd, sys
 
 class HBNBCommand(cmd.Cmd):
     """command line"""
+    prompt = "(hbnb)"
     def do_quit(self, arg):
         """write quit to exit"""
         try:
             sys.exit(int(arg))
         except(ValueError, TypeError):
             sys.exit(0)
+
+    def help_quit(self):
+        """ help doc """
+        print("Enter Quit to Exit program")
+    
+    def do_EOF(self, args):
+        """ Exit program on CTRL+D"""
+        print(args)
+        sys.exit(-1)
+    
+    def help_EOF(self):
+        """ hellp EOF """
+        print("Exit Program on CTRL+D")
+    
+    def emptyline(self):
+        """Overrides the emptyline without"""
+        pass
