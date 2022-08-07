65433d4829e8baef9f3259da2a8855218e920a11
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-08-07 15:45:42 +0200

    pycodestyle fix

--- console.py
+++ console.py
@@ -2,14 +2,17 @@
 """
 The main console
 """
-import cmd, sys
+import cmd
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
     """command line"""
     prompt = "(hbnb)"
+
     def do_quit(self, arg):
         """write quit to exit"""
+
         try:
             sys.exit(int(arg))
         except(ValueError, TypeError):
@@ -18,16 +21,16 @@ class HBNBCommand(cmd.Cmd):
     def help_quit(self):
         """ help doc """
         print("Enter Quit to Exit program")
-    
+
     def do_EOF(self, args):
         """ Exit program on CTRL+D"""
         print(args)
         sys.exit(-1)
-    
+
     def help_EOF(self):
         """ hellp EOF """
         print("Exit Program on CTRL+D")
-    
+
     def emptyline(self):
         """Overrides the emptyline without"""
         pass
